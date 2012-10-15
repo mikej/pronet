aae09472aa8003942c1b9b489efe96dd369cac08
Michael Josephson <mike@josephson.org>
2012-10-15 12:40:30 +0100

    Display a network in a table

---
+++ src/main/java/com/jax/teamcraft/pronet/FormatNetwork.java
@@ -0,0 +1,22 @@
+package com.jax.teamcraft.pronet;
+
+import org.apache.commons.lang.StringUtils;
+
+public class FormatNetwork {
+
+	public String formatNetwork(INetwork network) {
+		StringBuilder sb = new StringBuilder();
+		sb.append(formatLine("Name", "Skills", "Recommends"));
+		for (IProgrammer programmer : network.getProgrammers()) {
+			String skillList = StringUtils.join(programmer.getSkills(), ',');
+			String recommends = StringUtils.join(programmer.getRecommendations(), ',');
+			sb.append(formatLine(programmer.getName(), skillList, recommends));
+		}
+		return sb.toString();
+	}
+
+	private String formatLine(String f1, String f2, String f3) {
+		return String.format("%-10s %-20s %s\n", f1, f2, f3);
+	}
+	
+}
---
+++ src/main/java/com/jax/teamcraft/pronet/ShowNetwork.java
@@ -0,0 +1,17 @@
+package com.jax.teamcraft.pronet;
+
+public class ShowNetwork {
+
+	public static void main(String[] args) {
+		ShowNetwork showNetwork = new ShowNetwork();
+		showNetwork.run(args);
+	}
+
+	private void run(String[] args) {
+		String filename = args[0];
+		INetwork network = new Network();
+		network.load(filename);
+		System.out.println(new FormatNetwork().formatNetwork(network));
+	}
+	
+}
